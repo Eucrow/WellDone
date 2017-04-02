c895186541c2b96514e917ad5ce3662d017f8ece
Marco A. Amez <marco.a.amez@gmail.com>
2017-04-02 15:21:07 +0200

    Add signup and delete user endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from users.api import UserAPI
 from users.views import SignUpView, SignUpSuccessView, DeleteUserView
 from django.conf.urls import url
 
@@ -7,4 +8,8 @@ urlpatterns = [
     url(r'^signup$', SignUpView.as_view(), name='signup'),
     url(r'^signup_success$', SignUpSuccessView.as_view(), name='signup_success'),
     url(r'^delete_user/(?P<pk>[0-9]+)$', DeleteUserView.as_view(), name="delete_user"),
+
+    # api urls
+    url(r'^api/0.1/signup$', UserAPI.as_view(), name="signup_api"),
+    url(r'^api/0.1/delete_user/(?P<pk>[0-9]+)$', UserAPI.as_view(), name="delete_user_api")
 ]
