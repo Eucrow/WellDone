506f398747f2a87671ef68e440ace25213f53c0a
Marco A. Amez <marco.a.amez@gmail.com>
2017-04-29 11:39:49 +0200

    Begin with RabbitMQ

--- users/models.py
+++ users/models.py
@@ -1,3 +1,26 @@
+import pika
 from django.db import models
 
 # Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.conf import settings
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        print("Hulap")
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='hello')
+        channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body='Hello World!')
+        print(" [x] Sent 'Hello World!'")
+        connection.close()
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
