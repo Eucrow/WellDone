9ec8d2c95fca04ef5996483db10eba3f9a70d414
mdmarjaliza <manueldiazmarjaliza@gmail.com>
2017-05-30 16:55:46 +0200

    Modificaciones para guardar el username al crear un post en el microservicio de posts. Ahora mismo se puede ver el listado de posts general (home), el listado de posts de un usuario concreto y crear un post.

--- welldone/views.py
+++ welldone/views.py
@@ -52,7 +52,7 @@ class PostAPIView(ProxyView):
 class CreatePostAPIView(ProxyView):
     permission_classes = (IsAuthenticated,)
 
-    # Salto directamente al microservicio de post, para usar postman al crear no hace falta generar ninguna
+    # Salto directamente al microservicio de post, para usar postman al crear no hace falta generar ninguna web
     proxy_host = 'http://127.0.0.1:9002'
     source = 'api/1.0/posts/'
     return_raw = True
@@ -60,17 +60,8 @@ class CreatePostAPIView(ProxyView):
     def get_headers(self, request):
         headers = super(CreatePostAPIView, self).get_headers(request)
         if request.user.is_authenticated():
-            # user = {
-            #     "id": request.user.id,
-            #     "username": request.user.username,
-            #     "email": request.user.email,
-            #     "first_name": request.user.first_name,
-            #     "last_name": request.user.last_name,
-            #     "is_active": request.user.is_active,
-            #     "is_staff": request.user.is_staff,
-            #     "is_superuser": request.user.is_superuser
-            # }
             headers['Authorization'] = str(request.user.pk)
+            headers['X-Username'] = request.user.username
         return headers
 
 
